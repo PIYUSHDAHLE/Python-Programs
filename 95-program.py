3a1fd5d9f4bfb9a15b11fc4686afdefc573ddbae
Piyush Dahle <57090813+PIYUSHDAHLE@users.noreply.github.com>
2023-05-08 18:45:01 +0530

    Add files via upload

---
+++ 95-program.py
@@ -0,0 +1,11 @@
+import os
+os.system('cls')
+
+def factorial_iter(n):
+    product = 1
+    for i in range(n):
+        product = product * (i+1)
+    return product
+
+f = factorial_iter(5)
+print(f)
