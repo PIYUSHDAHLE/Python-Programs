fd7eeb950869f795b3c2412bd65cfc70bf51b5e2
Piyush Dahle <57090813+PIYUSHDAHLE@users.noreply.github.com>
2023-05-08 18:45:48 +0530

    Add files via upload

---
+++ 98-program.py
@@ -0,0 +1,17 @@
+import os
+os.system('cls')
+
+def max(n1,n2,n3):
+    if(n1>n2 and n1>n3):
+       return n1
+    elif(n2>n1 and n2>n3):
+        return n2
+    elif(n3>n1 and n3>n2):
+        return n3
+
+n1=input("enter the 1st number :")
+n2=input("enter the 2nd number :")
+n3=input("enter the 3rd number :")
+
+m=max(n1,n2,n3)
+print("The maximun number is "+str(m))
